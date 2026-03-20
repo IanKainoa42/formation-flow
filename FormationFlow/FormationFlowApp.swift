f39e12fe0e16f0d3f2c790671686f00dea06e6ef
Ian Kainoa Richardson <207976469+IanKainoa42@users.noreply.github.com>
2026-03-20 15:56:03 -0700

    feat: inject EntitlementManager as environment object

--- FormationFlow/FormationFlowApp.swift
+++ FormationFlow/FormationFlowApp.swift
@@ -2,10 +2,13 @@ import SwiftUI
 
 @main
 struct FormationFlowApp: App {
+    @StateObject private var entitlementManager = EntitlementManager()
+
     var body: some Scene {
         WindowGroup {
             RoutineWorkspaceView()
                 .tint(.orange)
+                .environmentObject(entitlementManager)
         }
     }
 }
